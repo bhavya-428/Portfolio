09ebe585e899be459bb7e77e22e163e7a66e2b77
Tanuja22hp1a4432 <tanujanemalikanti@gmail.com>
2026-03-04 17:12:36 +0530

    Integrate profile photo into About section and finalize resume details

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import './About.css';
+import profilePhoto from '../assets/profile.png';
 
 const About = () => {
   return (
@@ -39,8 +38,8 @@ const About = () => {
             <span className="stat-label">Diploma CGPA in Kallam Haranadha reddy institute of technology </span>
           </div>
 
-          <div className="about-letter-card glass reveal reveal-delay-3">
-            <span className="bg-letter">B</span>
+          <div className="about-photo-card glass reveal reveal-delay-3">
+            <img src={profilePhoto} alt="Bhavya Sri" className="about-profile-img" />
           </div>
 
           <div className="about-stat-card glass reveal reveal-delay-4">
